435a3e1ffd7ace2cf53504e306feb27bbeba12ed
elregalo <giftmoobi@gmail.com>
2022-07-10 14:27:17 +0200

    initial commit

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+/**
+ * @author - Moobi Kabelo
+ */
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+export { router as indexRouter };
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 /**
  * @author - Moobi Kabelo
- * @date - 2022-07-10
  */
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
 import ip from "ip";
-
+import { indexRouter } from "./routes/routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("views/public/assets"));
+app.set("view engine", "ejs");
+
+app.use(indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on: http://${ip.address()}:${PORT}`);
